25f0c1e7b1fdf72e6155856fea034c5f683a2c23
Owen Merkling <omerkling@users.noreply.github.com>
2020-02-01 11:22:07 -0700

    Add prefab brushes and scripts
    Script can iterate over the grid and print it as an example

---
+++ UnityProject/ZenGarden/Assets/Scripts/Puzzle/PuzzlePrinter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PuzzlePrinter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Print();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Print()
+    {
+        Tilemap tilemap = GetComponent<Tilemap>();
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null)
+                {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                } else
+                {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+            }
+        }
+    }
+}
