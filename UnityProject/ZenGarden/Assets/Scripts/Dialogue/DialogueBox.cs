6d2aefe1730d3d2c1ce9f146925507acfef38464
Kit Burgess <kburgess@gamedesignkit.net>
2020-01-31 22:50:04 -0700

    Simple Dialogue Box that can be fed text via function call;

---
+++ UnityProject/ZenGarden/Assets/Scripts/Dialogue/DialogueBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBox : MonoBehaviour
+{
+    public Text NameText;
+    public Text DialogueText;
+    public Button NextButton; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetDialogue("TestName", "TestDialogue");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetDialogue(string name, string dialogue) {
+        NameText.text = name;
+        DialogueText.text = dialogue;
+    }
+}
