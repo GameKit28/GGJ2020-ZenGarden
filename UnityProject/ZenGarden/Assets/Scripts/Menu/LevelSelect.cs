6e8ab28a836b76835be2d505674a5f4bf31d1d2b
DESKTOP-0S2RBUH\James <james.joseph@du.edu>
2020-02-02 14:16:19 -0700

    Grayscale stuff added

--- UnityProject/ZenGarden/Assets/Scripts/Menu/LevelSelect.cs
+++ UnityProject/ZenGarden/Assets/Scripts/Menu/LevelSelect.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GameState;
+using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
-    public GameObject manager;
+    //public GameObject manager;
     public GameObject[] levelButtons;
 
     private void Start()
     {
-        manager = GameObject.Find("GameManager");
+        //manager = GameObject.Find("GameManager");
         GameManager.Instance.GetScenesCompleted();
         levelButtons = GameObject.FindGameObjectsWithTag("LevelButton");
         for(int i = 0; i < levelButtons.Length; i++)
         {
             levelButtons[i].SetActive(false);
         }
+        levelButtons[0].SetActive(true);
         checkProgress();
     }
 
@@ -29,8 +31,13 @@ public class LevelSelect : MonoBehaviour
     {
         for(int i = 0; i < GameManager.Instance.GetScenesCompleted(); i++)
         {
-            levelButtons[i].SetActive(true);
+            levelButtons[i+1].SetActive(true);
         }
+    }
+
+    public void PressLevel1()
+    {
+        GameManager.Instance.LoadNewUnityScene("Story");
     }
 
 }
