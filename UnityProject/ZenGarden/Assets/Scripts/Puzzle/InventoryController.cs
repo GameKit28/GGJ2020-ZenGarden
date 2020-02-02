f26743ec90b5714b44e713931058f8a64b48e83d
Owen Merkling <omerkling@users.noreply.github.com>
2020-02-02 14:09:36 -0700

    Add level end splash

--- UnityProject/ZenGarden/Assets/Scripts/Puzzle/InventoryController.cs
+++ UnityProject/ZenGarden/Assets/Scripts/Puzzle/InventoryController.cs
@@ -2,20 +2,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameState;
 
 public class InventoryController : MonoBehaviour
 {
     PuzzleController puzzleController;
+    Tool[] tools;
+    private GameObject endLevelSplash;
+    public bool goToNextScene = true;
+
     // Start is called before the first frame update
     void Start()
     {
         puzzleController = GameObject.FindGameObjectWithTag("PuzzleController").GetComponent<PuzzleController>();
+        tools = gameObject.transform.GetComponentsInChildren<Tool>();
+
+        endLevelSplash = GameObject.FindGameObjectWithTag("EndLevelSplash");
+        endLevelSplash.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Array.TrueForAll(tools, tool => !tool.gameObject.activeInHierarchy || tool.count==0))
+        {
+            EndLevel();
+        }
         
+    }
+
+    public void EndLevel()
+    {
+        endLevelSplash.SetActive(true);
+        Invoke("GoToNextScene", 3);
+    }
+
+    public void GoToNextScene()
+    {
+        GameManager.Instance.LoadNewUnityScene("Story");
     }
 
     internal void SetToolSelected(Tool tool)
