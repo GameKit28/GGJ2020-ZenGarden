f739a0c888b09bf125d13544465589002431c1f8
Kit Burgess <kburgess@gamedesignkit.net>
2020-02-01 22:53:36 -0700

    Added new Sam character sprite; Added GameManager to save game state between Unity scene transitions;

---
+++ UnityProject/ZenGarden/Assets/Scripts/GameState/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Story.Model;
+using UnityEngine;
+
+namespace GameState
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+        
+        public List<StoryScene> StartingStoryScenes = new List<StoryScene>();
+        
+        private HashSet<StoryScene> CompletedStoryScenes = new HashSet<StoryScene>();
+        
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                GameObject.DontDestroyOnLoad(this);
+            }
+            else
+            {
+                GameObject.Destroy(this.gameObject);
+            }
+        }
+
+        public StoryScene GetNextScene()
+        {
+            foreach (StoryScene scene in StartingStoryScenes)
+            {
+                if (!CompletedStoryScenes.Contains(scene))
+                {
+                    return scene;
+                }
+            }
+
+            return null;
+        }
+
+        public void MarkSceneCompleted(StoryScene scene)
+        {
+            CompletedStoryScenes.Add(scene);
+        }
+
+        public bool HasCompletedAnyScenes()
+        {
+            return CompletedStoryScenes.Count > 0;
+        }
+    }
+}
