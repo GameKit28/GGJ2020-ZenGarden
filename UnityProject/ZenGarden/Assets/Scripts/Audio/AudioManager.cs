775ab38ce1383731b42a2249768fcb9d513ff78f
Kit Burgess <kburgess@gamedesignkit.net>
2020-02-02 17:00:25 -0700

    Audio manager added to Game Manager;

---
+++ UnityProject/ZenGarden/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        
+    }
+}
--- UnityProject/ZenGarden/Assets/Scripts/GameState/GameManager.cs
+++ UnityProject/ZenGarden/Assets/Scripts/GameState/GameManager.cs
@@ -14,7 +14,9 @@ namespace GameState
         
         private HashSet<StoryScene> CompletedStoryScenes = new HashSet<StoryScene>();
 
+        public AudioSource AudioSource;
 
+        public List<AudioClip> Sounds;
         
         private void Awake()
         {
@@ -27,6 +29,11 @@ namespace GameState
             {
                 GameObject.Destroy(this.gameObject);
             }
+        }
+
+        private void Start()
+        {
+            AudioSource = GetComponent<AudioSource>();
         }
 
         public StoryScene GetNextScene()
@@ -61,6 +68,19 @@ namespace GameState
         {
             Debug.Log("Loading New Unity Scene: " + sceneName);
             SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+
+        public void PlaySoundClip(string name)
+        {
+            AudioClip clipToPlay = null;
+            foreach (AudioClip clip in Sounds)
+            {
+                if (clip.name == name)
+                {
+                    clipToPlay = clip;
+                }
+            }
+            AudioSource.PlayOneShot(clipToPlay);
         }
     }
 }
