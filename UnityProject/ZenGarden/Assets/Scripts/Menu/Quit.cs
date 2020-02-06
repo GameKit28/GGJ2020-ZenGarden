bd60217058ae70f5d38e9fca4595b15da39dfc51
Owen Merkling <omerkling@users.noreply.github.com>
2020-02-06 10:18:17 -0700

    Add exit button

---
+++ UnityProject/ZenGarden/Assets/Scripts/Menu/Quit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void Awake()
+    {
+#if UNITY_WEBPLAYER
+        gameObject.SetActive(false);             
+#endif
+    }
+    public void OnQuitPressed()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
